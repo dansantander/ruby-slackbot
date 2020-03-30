3caf0be2052f97db5fa2c66a83b4a451a9a496b8
Daniel Santander <danyvo1d@gmail.com>
2020-03-30 14:48:04 -0600

    Updated get_news.rb methods

--- lib/bot.rb
+++ lib/bot.rb
@@ -2,12 +2,12 @@ module GetNews
   class Bot < SlackRubyBot::Bot
     help do
       title 'News Bot'
-      desc 'This bot gives you a random movie from fmovies.to'
+      desc 'This bot gives you a random news from CNN International'
 
       command :get_news do
         title 'get_news'
-        desc 'Return the url of a random cnn world news'
-        long_desc 'Return the url of a random cnn world news'
+        desc 'Return the url of a random CNN World news'
+        long_desc 'Return the url of a random CNN World news'
       end
     end
   end
--- lib/commands/get_news.rb
+++ lib/commands/get_news.rb
@@ -12,6 +12,30 @@ module News
         client.say(channel: data.channel, text: rss.link)
       end
 
+      command 'get_politics' do |client, data, _match|
+        url = 'https://rss.app/feeds/Dk0X526oGrL6c2Rv.xml'
+        rss = RSS::Parser.parse(open(url).read, false).items.sample
+        client.say(channel: data.channel, text: rss.link)
+      end
+
+      command 'get_bussiness' do |client, data, _match|
+        url = 'https://rss.app/feeds/tssARKhZatlCihF1.xml'
+        rss = RSS::Parser.parse(open(url).read, false).items.sample
+        client.say(channel: data.channel, text: rss.link)
+      end
+
+      command 'get_entertainment' do |client, data, _match|
+        url = 'https://rss.app/feeds/P595TYafGAXO3td4.xml'
+        rss = RSS::Parser.parse(open(url).read, false).items.sample
+        client.say(channel: data.channel, text: rss.link)
+      end
+
+      command 'get_health' do |client, data, _match|
+        url = 'https://rss.app/feeds/FD3XTd2WJAPoVzjl.xml'
+        rss = RSS::Parser.parse(open(url).read, false).items.sample
+        client.say(channel: data.channel, text: rss.link)
+      end
+
       command 'say_hello' do |client, data, _match|
         client.say(channel: data.channel, text: HelloText.say_hello)
       end
