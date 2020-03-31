c1f252e0878b6a116786b7bfe60d2a3df1f05750
Daniel Santander <danyvo1d@gmail.com>
2020-03-31 11:55:06 -0600

    Added spec file

--- lib/commands/get_news.rb
+++ lib/commands/get_news.rb
@@ -1,6 +1,7 @@
 require 'rss'
 require 'open-uri'
 require 'json'
+require_relative 'topic.rb'
 
 module News
   module Commands
@@ -26,29 +27,6 @@ module News
         end
       end
 
-    end
-  end
-end
-
-class Topic
-  def initialize
-    @valid_topics = {
-      world: "https://rss.app/feeds/aPyh7dgZjMnh7LeP.xml",
-      politics: "https://rss.app/feeds/Dk0X526oGrL6c2Rv.xml" ,
-      bussiness: "https://rss.app/feeds/tssARKhZatlCihF1.xml",
-      entertainment: "https://rss.app/feeds/P595TYafGAXO3td4.xml",
-      health: "https://rss.app/feeds/FD3XTd2WJAPoVzjl.xml"
-    }
-  end
-
-  def search_topic(topic)
-    topic.downcase!
-    @valid_topics.each do |k, v|
-      if topic == k.to_s
-        return v
-      else
-        return nil
-      end
     end
   end
 end
